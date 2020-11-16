f8caaab2ce3ca8c8620903dfd866c9ea61afc092
Slowthy Sloth <slothychemdoksloth@gmail.com>
2020-11-16 16:17:16 +0530

    Changed joke command source again

--- commands/joke.js
+++ commands/joke.js
@@ -1,16 +1,16 @@
 const Discord = require("discord.js");
 const generateRandomColor = require("../extras/generateRandomColor");
-var myjoke = require("make-me-laugh");
+const oneLinerJoke = require("one-liner-joke");
 
 const joke = (message) => {
-  var joke = myjoke.getjoke();
+  var getRandomJoke = oneLinerJoke.getRandomJoke();
   const user = message.mentions.users.first() || message.author;
 
   const color = "#" + generateRandomColor();
   const finalResponse = new Discord.MessageEmbed()
     .setColor(color)
     .setTitle("Here's A Joke For You " + user.username)
-    .setDescription(joke);
+    .setDescription(getRandomJoke.body);
   message.channel.send(finalResponse);
 };
 
