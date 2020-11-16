6eac6b1fbf826352296eaa97928310f70a317f3f
Slowthy Sloth <slothychemdoksloth@gmail.com>
2020-11-16 15:55:53 +0530

    Changed joke command source

--- commands/joke.js
+++ commands/joke.js
@@ -1,20 +1,14 @@
-const fetch = require("node-fetch");
 const Discord = require("discord.js");
 const generateRandomColor = require("../extras/generateRandomColor");
+var myjoke = require("make-me-laugh");
 
-const joke = async (message) => {
-  const url = "https://sv443.net/jokeapi/v2/joke/Any?format=txt";
-  async function getData(url) {
-    const response = await fetch(url);
+const joke = (message) => {
+  var joke = myjoke.getjoke();
 
-    return response.text();
-  }
-
-  const jokeText = await getData(url);
   const color = "#" + generateRandomColor();
   const finalResponse = new Discord.MessageEmbed()
     .setColor(color)
-    .setDescription(jokeText);
+    .setDescription(joke);
   message.channel.send(finalResponse);
 };
 
