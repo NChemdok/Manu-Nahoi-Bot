6a9a27d274ede3ae56ff0c3195514da721d5f388
Slowthy Sloth <slothychemdoksloth@gmail.com>
2020-11-17 22:12:13 +0530

    added long text support to spampls

--- commands/spampls.js
+++ commands/spampls.js
@@ -1,7 +1,8 @@
 const spampls = (message) => {
   let messageText = message.content.slice(8).trim();
-  var [textToSpam, noOfTimes] = messageText.split(" ");
+  var [textToSpam, noOfTimes] = messageText.split("-");
   const times = parseInt(noOfTimes);
+  console.log(times);
   for (var i = 0; i < times; i++) {
     // message.channel.send(
     //   characters.charAt(Math.floor(Math.random() * charactersLength))
