a10a03f4fd909a3779805d1982c7692b3c703938
Slowthy Sloth <slothychemdoksloth@gmail.com>
2021-10-09 22:56:09 +0530

    removed pause and resume command node version issue

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -1,14 +1,16 @@
 const pause = async (message, serverQueue) => {
-  if (!message.member.voice.channel) {
-    return message.channel.send(
-      "You have to be in a voice channel to skip the music!"
-    );
-  }
-  if (!serverQueue) {
-    return message.channel.send("Bot not playing currently");
-  }
-  serverQueue.connection.dispatcher.pause();
-  return message.channel.send("Playback Paused");
+  // if (!message.member.voice.channel) {
+  //   return message.channel.send(
+  //     "You have to be in a voice channel to skip the music!"
+  //   );
+  // }
+  // if (!serverQueue) {
+  //   return message.channel.send("Bot not playing currently");
+  // }
+  // serverQueue.connection.dispatcher.pause();
+  return message.channel.send("Not Enabled for now").then((msg) => {
+    setTimeout(() => msg.delete({ timeout: 4000 }));
+  });
 };
 
 module.exports = pause;
